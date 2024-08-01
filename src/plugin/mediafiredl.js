af1a48ed8a983063681419e4b5cdb23ad62adce4
Vishwa Mihiranga <145928580+vishwamihiranga@users.noreply.github.com>
2024-08-01 09:31:26 +0530

    Update mediafiredl.js

--- src/plugin/mediafiredl.js
+++ src/plugin/mediafiredl.js
@@ -20,7 +20,7 @@ const mediafireDownload = async (m, Matrix) => {
 
       if (mediafireInfo && mediafireInfo.link) {
         const mediaUrl = mediafireInfo.link;
-        const caption = `> © Powered By Ethix-Xsid\n> File: ${mediafireInfo.name}\n> Size: ${mediafireInfo.size}\n> Date: ${mediafireInfo.date}`;
+        const caption = `> ©ᴘᴏᴡᴇʀᴅ ʙʏ ᴠɪꜱʜᴡᴀ-ᴍᴅ\n> File: ${mediafireInfo.name}\n> Size: ${mediafireInfo.size}\n> Date: ${mediafireInfo.date}`;
 
 
         const extension = mediaUrl.split('.').pop().toLowerCase();
